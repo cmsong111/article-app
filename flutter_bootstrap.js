790ad577be979f9b91d0569a90a751f10c5eb5ee
cmsong111 <cmsong111@users.noreply.github.com>
2025-05-20 12:26:19 +0000

    deploy: a971e68ed240cc5a8c8be9ab50d73ddaff448786

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3269843415"
+    serviceWorkerVersion: "1283479470"
   }
 });
